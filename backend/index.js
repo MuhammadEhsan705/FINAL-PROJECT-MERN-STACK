0d9daeb376f03798a7307afb4cafcf8cc5540400
MuhammadEhsan705 <muhammadehsanali103@gmail.com>
2026-03-17 22:59:44 -0700

    fixing deployment

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ connectCloudinary()
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://final-project-mern-stack.vercel.app",
       credentials: true,
     })
   );
