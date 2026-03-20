d06d91c760a848109959d82854d3e87a3e2d22a2
MuhammadEhsan705 <muhammadehsanali103@gmail.com>
2026-03-19 21:50:16 -0700

    change axios

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ connectCloudinary()
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "http://localhost:5174",
       credentials: true,
     })
   );
