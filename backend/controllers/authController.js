3645335d47de49d8115721639f21cfd573489121
MuhammadEhsan705 <muhammadehsanali103@gmail.com>
2026-03-21 19:22:56 -0700

    change none genrate token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,7 @@ const GenrateToken=(res,payload)=>{
     res.cookie("token",token,{
         httpOnly:true,
         secure:process.env.NODE_ENV ==="production",
-        sameSite:"None",
+        sameSite:"none",
         maxAge:24*60*60*1000
     });
     return token;
